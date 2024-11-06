d8072bf41fcdedd362fca12d64613f895e3faec0
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2024-11-06 15:06:09 +0100

    test

--- gpu_runner.sh
+++ gpu_runner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-x11vnc -geometry 1200x860 -forever -rfbauth /app/.vnc/passwd -usepw -display :99
+/usr/lib/xorg/Xorg -core :1
+x11vnc -geometry 1200x860 -forever -rfbauth /app/.vnc/passwd -usepw -display :1
 blender

399a286afa1a5fa9a5d2a7053e3a97189b430dd4
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2024-11-07 15:03:52 +0100

    test

--- gpu_runner.sh
+++ gpu_runner.sh
@@ -3,5 +3,8 @@
 startx
 #/usr/lib/xorg/Xorg -core :1
 #x11vnc -geometry 1200x860 -forever -rfbauth /app/.vnc/passwd -usepw -display :1
-x11vnc -display :1 -geometry 1200x860 -forever -rfbport 5900 -timeout 300 -accept popup -passwd pass12321
+/usr/bin/x11vnc -storepasswd pass12321 /etc/x11vnc.pass
+/usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -rfbport 5900 -display :0 -auth guess -rfbauth /etc/x11vnc.pass
+#x11vnc -display :1 -geometry 1200x860 -forever -rfbport 5900 -timeout 300 -accept popup -passwd pass12321
+echo "running blender..."
 blender

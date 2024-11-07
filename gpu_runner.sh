49f44b407d40173bbe0fee5fb2f2b7b4ff65973b
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2024-11-07 12:08:57 +0100

    vnc pass

--- gpu_runner.sh
+++ gpu_runner.sh
@@ -2,5 +2,6 @@
 
 startx
 #/usr/lib/xorg/Xorg -core :1
-x11vnc -geometry 1200x860 -forever -rfbauth /app/.vnc/passwd -usepw -display :1
+#x11vnc -geometry 1200x860 -forever -rfbauth /app/.vnc/passwd -usepw -display :1
+x11vnc -display :1 -geometry 1200x860 -forever -rfbport 5900 -timeout 300 -accept popup -passwd pass12321
 blender
